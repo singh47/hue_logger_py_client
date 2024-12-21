b9d614491ab7d5a65820b4dc7da30ee819864b4c
singh47 <bravesimran@gmail.com>
2024-12-21 12:07:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hue Logger
+
+Hue Logger is a simple Python client for sending logs to the Hue Logs server. Hue Logs is minimal logging dashboard.
+
+## Installation
+
+```bash
+pip install hue-logger
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hue_logger",
+    version="0.1.0",
+    author="Harman Singh",
+    author_email="harman@petdrifts.com",
+    description="A simple client for sending logs to the Hue Logs server.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/singh247/hue_logger_py_client",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
