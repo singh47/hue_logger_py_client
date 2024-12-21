6cfbb5e6fde9c2c498f7ecce710f889111594cb5
singh47 <bravesimran@gmail.com>
2024-12-21 14:02:25 -0600

    updated license

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
     description="A simple client for sending logs to the Hue Logs server.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/singh47/hue_logger_py_client",
+    url="https://github.com/singh47/hue_logger_py_client", 
     packages=find_packages(),
     install_requires=[
         "requests>=2.0.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
